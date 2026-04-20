da6008b936c7f6134931bae5b4537f7cf7605a37
pawanmm <pawanmm.2011@gmail.com>
2026-04-20 23:45:35 +0530

    first commit

---
+++ src/main/java/com/pawan/paymentserviceecom/PaymentServiceEcomApplication.java
@@ -0,0 +1,13 @@
+package com.pawan.paymentserviceecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceEcomApplication.class, args);
+    }
+
+}
